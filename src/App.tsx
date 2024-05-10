4b7493e1d344554d389743d1518410b978ed93e2
ainizoda <ainizodam@gmail.com>
2024-05-11 01:36:03 +0500

    remove unused imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,8 @@
-import { useCallback, useState } from "react";
+import { useState } from "react";
 import { Container, InputBox, Form } from "./components";
 import { useDebounce, useThrottle } from "./hooks";
 
 import "./App.scss";
-import { AnyFunc } from "./hooks/types";
 
 function App() {
   const [deboundVal, setDeboundVal] = useState("");
